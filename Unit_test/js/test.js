24faf05faf0ac4b24cc682109e9b9d9b8ea340a4
Anatoly_Chernyshev <kayn23@yandex.ru>
2018-10-23 02:56:55 +0300

    просто повторяю что есть на уроке. пытаюсь просто вникнуть и заодно сниппеты тестирую

--- Unit_test/js/test.js
+++ Unit_test/js/test.js
@@ -3,3 +3,13 @@ describe('Проверка умножения',()=> {
         assert.equal(multi(7,8),56)
     })
 });
+describe('проверка api', () => {
+    var list;
+    before(async () => {
+        list = await fetch('https://dog.ceo/api/breeds/list/all')
+            .then(req => req.json());
+    });
+    it('есть ли будьдог', () => {
+        assert.isDefined(list.message.bulldog);
+    });
+});
