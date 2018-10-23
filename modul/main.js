c0e8bdb5e07ab947caa7b24afe4ef28b0d7303e7
Anatoly_Chernyshev <kayn23@yandex.ru>
2018-10-24 00:27:25 +0300

    пришлось переделать под es5

--- modul/main.js
+++ modul/main.js
@@ -1,47 +1,55 @@
-class Tax {
-    constructor(name, value, currency) {
-      this.name = name;
-      this.value = value;
-      this.currency = currency;
-    }
-  }
-  
-  class Parser {
-    regOldTax  {
-          regGeneralTax: /\D{2}\d{2}\s\D{3}\s+\w+/gmi,
-          regCurrencyTax: /(?<=.{5})[a-z]{3}/gmi,
-          regValueTax: /(?<=\s)[0-9]+/gmi,
-          regNameTax: /\w{2}(?=$)/gmi
-        };
-    regNewTax= {
-          newTax: /\w{3}\s+\w+\-\w{2}/gi
-        };
-  
-      parse = (str,reg) => str.match(reg)
-      createNewTax = (item) => {
-        return item.map((item)=>{
-              var elem = new Tax(
-                  this.parse(item,this.regOldTax.regNameTax),
-                    this.parse(item,this.regOldTax.regValueTax),
-                    this.parse(item,this.regOldTax.regCurrencyTax)
-              );
-                return elem;
-          });
-      }
-      getOldTax = (string) => {
-           var arrTax = [];
-            arrTax = this.parse(string,this.regOldTax.regGeneralTax);
-        console.log(arrTax);
-            arrTax = this.createNewTax(arrTax)
-        return arrTax
-      }
-      getNewTax = (string) => {
-        var arrTax = [];
-        arrTax = this.parse(string,this.regNewTax.newTax);
-        arrTax = this.createNewTax(arrTax)
-        return arrTax
-      }
-  }
-  
-  
-  var parser = new Parser;
+"use strict";
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var Tax = function Tax(name, value, currency) {
+  _classCallCheck(this, Tax);
+
+  this.name = name;
+  this.value = value;
+  this.currency = currency;
+};
+
+var Parser = function Parser() {
+  var _this = this;
+
+  _classCallCheck(this, Parser);
+
+  this.regOldTax = {
+    regGeneralTax: /\D{2}\d{2}\s\D{3}\s+\w+/gmi,
+    regCurrencyTax: /(?<=.{5})[a-z]{3}/gmi,
+    regValueTax: /(?<=\s)[0-9]+/gmi,
+    regNameTax: /\w{2}(?=$)/gmi
+  };
+  this.regNewTax = {
+    newTax: /\w{3}\s+\w+\-\w{2}/gi
+  };
+
+  this.parse = function (str, reg) {
+    return str.match(reg);
+  };
+
+  this.createNewTax = function (item) {
+    return item.map(function (item) {
+      var elem = new Tax(_this.parse(item, _this.regOldTax.regNameTax), _this.parse(item, _this.regOldTax.regValueTax), _this.parse(item, _this.regOldTax.regCurrencyTax));
+      return elem;
+    });
+  };
+
+  this.getOldTax = function (string) {
+    var arrTax = [];
+    arrTax = _this.parse(string, _this.regOldTax.regGeneralTax);
+    console.log(arrTax);
+    arrTax = _this.createNewTax(arrTax);
+    return arrTax;
+  };
+
+  this.getNewTax = function (string) {
+    var arrTax = [];
+    arrTax = _this.parse(string, _this.regNewTax.newTax);
+    arrTax = _this.createNewTax(arrTax);
+    return arrTax;
+  };
+};
+
+var parser = new Parser();
